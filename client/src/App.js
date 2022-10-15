ab92d90064ba32cef6cac6e83e91a8bdda97f53b
Ahmed Hassan <ah2k.dev@gmail.com>
2022-10-15 15:37:17 +0500

    Base setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
